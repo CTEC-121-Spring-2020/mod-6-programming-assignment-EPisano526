9182f5fee738e152977d42013402b752aaacf548
Esther Pisano <estherpisano@Esthers-MBP.domain>
2020-06-01 22:06:35 -0700

    one bonus Q finished

--- Prob-2/Prob-2.py
+++ Prob-2/Prob-2.py
@@ -2,9 +2,34 @@
 #   Programming Assignment 10
 #     Prob-2.py
 
-# <YOUR NAME>
+# <Esther Pisano
+
 
 def main():
-    # your code here
+    # f is the file read variable/label
+    f = open("Prob-2-Input.txt", "r")
+    # prints the whole file
+    # print(f.read())
 
-main()    
+    # this will hold all the numbers
+    all_num = []
+
+    # prints file line by line
+    for line in f:
+        # get the numbers into another list
+        line_nums = line.split()
+        # append that list to the mother list
+
+        #print("adding the list: ", line, " to all_num: ", all_num, "to get : ", all_num + line_nums, "\n\n")
+        all_num = all_num + line_nums
+
+    sum = 0
+
+    for num in all_num:
+        print(num)
+        sum = sum + int(num)
+
+    print("total sum: ", sum)
+
+
+main()
